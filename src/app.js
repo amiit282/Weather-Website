144989447d99ad6e757918b6ac0a649de4f6e843
Amit <amiit282@gmail.com>
2019-05-11 01:35:05 +0530

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+const hbs=require('hbs')
+
+//Define path for Express Config
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath=path.join(__dirname,'../Templates/views')
+const partialsPath=path.join(__dirname,'../Templates/partials')
+
+
+app.set('view engine', 'hbs')
+app.set('views',viewPath)
+hbs.registerPartials(partialsPath)
+app.use(express.static(publicDirectoryPath))
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help Page'
+    })
+})
+ 
+app.get('*',(req,res)=>{
+    res.send('My 404 Page')
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
