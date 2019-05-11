4b110256307e9dbfeed516837e800098eb65120e
Amit <amiit282@gmail.com>
2019-05-12 01:19:24 +0530

    Heroku

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ const express = require('express')
 const hbs=require('hbs')
 
 //Define path for Express Config
-const app = express()
+const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewPath=path.join(__dirname,'../Templates/views')
 const partialsPath=path.join(__dirname,'../Templates/partials')
-
+const port=process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 app.set('views',viewPath)
@@ -22,7 +22,8 @@ app.get('/help',(req,res)=>{
  
 app.get('*',(req,res)=>{
     res.send('My 404 Page');
+    
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port.'+port)
 })
